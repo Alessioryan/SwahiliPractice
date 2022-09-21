0e9fc9f7b0583258c63530b0cb5cad6cb8b80310
Tosolini <tosolini@uw.edu>
2022-09-20 17:17:32 -0700

    Able to interpret positive human Swahili subjects :)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-
-function test(Name){
-  alert("Test worked " + Name);
+const Vowels = ["a","e","i","o","u"];
+const Subjects = {
+  s1SG_POS: "ni",
+  s2SG_POS: "u",
+  s3SG_POS: "a",
+  s1PL_POS: "tu",
+  s2PL_POS: "m",
+  s3PL_POS: "wa"
 }
+
+function inputText() {
+  let input = document.getElementById("Verb").value;
+
+  document.getElementById("subj").innerHTML = findSubj(input);
+
+}
+
+
+function findSubj(verb) {
+  for(let subject of Object.keys(Subjects) ){
+    if(Subjects[subject] === verb.substring(0, Subjects[subject].length) ){
+      return Subjects[subject];
+    }
+  }
+  return "subject not found";
+}
+
+
+
+
+
